52dc668ac931be7365c82a112400123457c5185c
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-05-17 21:43:25 +0300

    Done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,59 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def get_title(self):
+        return self._title
+    
+    def set_title(self, title):
+        self._title = title
+    
+    title = property(get_title, set_title)
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count(self, page_count):
+        if (type(page_count) is int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    page_count = property(get_page_count, set_page_count)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+# class Book:
+#     def __init__(self, title, page_count):
+#         self.title = title
+#         self.page_count = page_count
+
+#     @property
+#     def title(self):
+#         return self._title
+
+#     @title.setter
+#     def title(self, value):
+#         self._title = value
+
+#     @property
+#     def page_count(self):
+#         return self._page_count
+
+#     @page_count.setter
+#     def page_count(self, value):
+#         if isinstance(value, int):
+#             self._page_count = value
+#         else:
+#             print("page_count must be an integer")
+
+#     def turn_page(self):
+#         print("Flipping the page...wow, you read fast!")
+
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
+
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+    
+    def get_brand(self):
+        return self.get_brand
+    
+    def set_brand(self, brand):
+        self.get_brand = brand
+
+    brand = property(get_brand, set_brand)
+
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if type(size) is int:
+            self._size = size
+        else:
+            print("size must be an integer")
+    
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        print ("Your shoe is as good as new!")
+        self.condition = "New"
+
